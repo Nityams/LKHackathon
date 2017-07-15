dec252863bc4ecdb167584c6458d7076281ef567
Nhu Luong <nhuluong@Nhus-MacBook-Pro.local>
2017-07-15 06:38:28 -0700

    Added more xml

---
+++ app/src/main/java/hackmesenpai/com/lkhackathon/Auction_Detail_Buyer.java
@@ -0,0 +1,23 @@
+package hackmesenpai.com.lkhackathon;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import java.io.File;
+
+/**
+ * Created by nhuluong on 7/15/17.
+ */
+
+public class Auction_Detail_Buyer extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.auction_detail_buyer);
+
+
+
+    }
+}
--- app/src/main/java/hackmesenpai/com/lkhackathon/HomePage.java
+++ app/src/main/java/hackmesenpai/com/lkhackathon/HomePage.java
@@ -1,13 +1,33 @@
 package hackmesenpai.com.lkhackathon;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        TextView text = (TextView)findViewById(R.id.user_name);
+        text.setTextColor(Color.BLACK);
+        text.setTextSize(12);
+        text.setText("Android Bot");
+
+        Button list = (Button) findViewById(R.id.listing);
+        list.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v) {
+        Log.i("clicks","You Clicked B1");
+        Intent i=new Intent(HomePage.this, Auction_Detail_Seller.class);
+        startActivity(i);
     }
 }
