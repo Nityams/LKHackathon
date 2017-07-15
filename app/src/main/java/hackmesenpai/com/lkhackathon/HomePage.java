ea96a1a0bfca43522eb35af071067f7ecdd381d5
Nityams <nityamshrestha@gmail.com>
2017-07-15 03:53:46 -0700

    master

--- app/src/main/java/hackmesenpai/com/lkhackathon/HomePage.java
+++ app/src/main/java/hackmesenpai/com/lkhackathon/HomePage.java
@@ -1,7 +1,11 @@
 package hackmesenpai.com.lkhackathon;
 
+import android.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePage extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        android.support.v4.app.Fragment frag = new Frag();
+
+
+//        fragmentTransaction.replace(R.id.fragment_container, newFragment).commit();
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frag_container, frag)
+                .commit();
+    }
+
+
+    public void upload(View view) {
+    }
+
+    public void userProfile(View view) {
     }
 }
